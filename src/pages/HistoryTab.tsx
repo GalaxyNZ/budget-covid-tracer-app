c006983f854d46b21fb6a0647a9f8f96adad7b61
Luke Catherall <catherluke@ecs.vuw.ac.nz>
2021-10-23 15:57:00 +1300

    Location History Screen Added

--- src/pages/HistoryTab.tsx
+++ src/pages/HistoryTab.tsx
@@ -4,59 +4,106 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardContent,
-  IonGrid,
-  IonRow,
-  IonCol,
+  IonItem,
+  IonLabel,
+  IonDatetime,
+  IonInput,
+  IonList,
   IonButton,
+  useIonViewWillEnter,
 } from "@ionic/react";
-import React, { useState, useEffect } from "react";
-import { Bar } from "react-chartjs-2";
-import firebase from "../firebase";
+import React, { useState, useEffect, useContext } from "react";
 import "./HistoryTab.css";
+import { Geolocation, Position } from "@capacitor/geolocation";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from "leaflet";
+import firebase from "../firebase";
+import UserContext from "../userContext";
+import { useHistory } from "react-router";
 
-const Tab3: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Coronavirus Dashboard</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonCard color="light">
-          <IonCardHeader>
-            <IonCardTitle>Latest Situation per </IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <IonGrid>
-              <IonRow>
-                <IonCol size="12" size-sm>
-                  Confirmed Cases: <strong>12</strong>
-                </IonCol>
-                <IonCol size="12" size-sm></IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol size="12" size-sm>
-                  Death: <strong>12</strong>
-                </IonCol>
-                <IonCol size="12" size-sm></IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol size="12" size-sm>
-                  Recovered: <strong>12</strong>
-                </IonCol>
-                <IonCol size="12" size-sm></IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonCardContent>
-        </IonCard>
-      </IonContent>
-    </IonPage>
-  );
+import "../../node_modules/leaflet/dist/leaflet.css";
+import "../../node_modules/leaflet/dist/leaflet.js";
+
+const Tab2: React.FC = () => {
+  const userContext = useContext(UserContext);
+  let history = useHistory();
+
+  const [entities, setEntities] = useState(new Array());
+
+  const locationsDB = firebase.firestore().collection("locations");
+
+  useEffect(() => {
+    const unsub = locationsDB
+      .where("authorID", "==", userContext.id)
+      .orderBy("createdAt", "desc")
+      .onSnapshot(
+        (querySnapshot) => {
+          var newEntities = new Array();
+          querySnapshot.forEach((doc) => {
+            const entity = doc.data();
+            entity.id = doc.id;
+            newEntities.push(entity);
+          });
+          setEntities(newEntities);
+          console.log(entities);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  if (entities.length == 0) {
+    return <IonLabel>Loading...</IonLabel>;
+  } else {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Locations</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Tab 2</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+              <div id="mapid">
+                <MapContainer
+                  dragging={true}
+                  zoom={15}
+                  scrollWheelZoom={true}
+                  style={{ height: "100%" }}
+                  center={[entities[0].latitude, entities[0].longitude]}
+                >
+                  <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  {entities.map((data: any, index: number) => (
+                    <Marker
+                      position={[data.latitude, data.longitude]}
+                      icon={
+                        new Icon({
+                          iconUrl: markerIconPng,
+                          iconSize: [18, 30],
+                          iconAnchor: [9, 30],
+                        })
+                      }
+                    ></Marker>
+                  ))}
+                </MapContainer>
+              </div>
+        </IonContent>
+      </IonPage>
+    );
+  }
 };
 
-export default Tab3;
+export default Tab2;
